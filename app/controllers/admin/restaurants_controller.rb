d9b5e6a09d60359570e95c5387ff42c54158be9b
jiananWang <a0918666589@gmail.com>
2018-01-15 21:42:13 +0800

    move authenticate_user! to application_controller.rb

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,6 +1,4 @@
 class Admin::RestaurantsController < ApplicationController
-  # 先請使用者登入，不然直接判斷admin程式會掛掉
-  before_action :authenticate_user!
   #這使用來判斷使同者是管理員還是一般使用者
   before_action :authenticate_admin
 
