2a87855bacb4be2e1fc941b74d526fb646a4fff3
jiananWang <a0918666589@gmail.com>
2018-01-24 22:56:04 +0800

    validate uniqueness of following_id in user_id scope

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,4 +1,7 @@
 class Followship < ApplicationRecord
+  # 在特定的user_id下，只能有一個·following_id
+  validates_uniqueness_of :following_id, scope: :user_id
+
   belongs_to :user
   belongs_to :following, class_name: "User"
 end
